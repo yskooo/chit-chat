75c48442ed3f29456b25c9f6ea864ec37513b320
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2022-10-25 20:35:56 +0800

    chore+

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {useAuthState} from 'react-firebase-hooks/auth'
 
 const style = {
   appContainer:`max-w-[720px] mx-auto text-center`,
-  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-200 mt-10 shadow-xl border relative`
 }
 
 function App() {
