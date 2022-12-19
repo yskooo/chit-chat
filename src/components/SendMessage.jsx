c59e33307c031367ae79f93bb76b444e0c004ce5
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2022-12-19 11:24:33 +0800

    chore: enhance color

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -5,7 +5,7 @@ import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 const style = {
     form: `h-14 w-full max-w[720px] flex text-xl absolute bottom-0`,
     input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
-    button: `w-[20%] bg-green-500`
+    button: `w-[20%] bg-green-600`
 }
 
 const SendMessage = ({scroll}) => {
